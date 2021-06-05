22ac11fe9b829b00496c492a2ef4714f6d519c8b
benl95 <beno_1995@hotmail.com>
2021-06-05 14:16:50 +0200

    refactor: make path enviroment variable

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const fs = require('fs');
 
 class Database {
 	create(data) {
 		const json = JSON.stringify(data);
-		return fs.writeFileSync(`./src/db/users.json`, json, err => {
+		return fs.writeFileSync(process.env.DB_PATH, json, err => {
 			if (err) return console.error(err);
 		});
 	}
