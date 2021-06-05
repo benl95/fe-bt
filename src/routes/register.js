e8c76aa7f9b2fc39d1549897b4367e4706498619
benl95 <beno_1995@hotmail.com>
2021-06-05 11:26:16 +0200

    feat: redirect user to success page after registration

--- src/routes/register.js
+++ src/routes/register.js
@@ -4,8 +4,13 @@ router.get('/register', (req, res) => {
 	res.render('register', { title: 'Register an account' });
 });
 
+router.get('/register/success', (req, res) => {
+	res.render('success', { title: 'Successfully registered' });
+});
+
 router.post('/register', (req, res) => {
 	console.log(req.body);
+	res.redirect('/register/success');
 });
 
 module.exports = router;
