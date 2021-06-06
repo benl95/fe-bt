8f1f8c4d98266f649d67b66c95511e5bc17e286d
benl95 <beno_1995@hotmail.com>
2021-06-06 09:42:40 +0200

    feat: create read method for db class

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -3,10 +3,18 @@ const fs = require('fs');
 
 class Database {
 	create(data) {
-		const json = JSON.stringify(data);
+		let database = this.read();
+		database.push(data);
+		const json = JSON.stringify(database);
 		return fs.writeFileSync(process.env.DB_PATH, json, err => {
 			if (err) return console.error(err);
 		});
+	}
+
+	read() {
+		let json = fs.readFileSync(process.env.DB_PATH);
+		let data = JSON.parse(json);
+		return data;
 	}
 }
 
