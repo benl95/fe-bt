70cfde379c9c25b716f6562dcde9238f00c64977
benl95 <beno_1995@hotmail.com>
2021-06-20 21:16:04 +0200

    chore: create main func for server

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
-// Import libraries
-require('dotenv').config();
-const express = require('express');
-const hbs = require('express-handlebars');
-const path = require('path');
-const app = express();
+const main = () => {
+	// Import libraries
+	require('dotenv').config();
+	const express = require('express');
+	const hbs = require('express-handlebars');
+	const path = require('path');
+	const app = express();
 
-// Port
-const port = process.env.PORT || 3000;
+	// Port
+	const port = process.env.PORT || 3000;
 
-// Routes
-const home = require('./src/routes/home');
-const register = require('./src/routes/register');
+	// Routes
+	const home = require('./src/routes/home');
+	const register = require('./src/routes/register');
 
-// Set view engine
-app.set('view engine', 'hbs')
-	.set('views', __dirname + '/views')
-	.engine('hbs', hbs({ extname: 'hbs', defaultLayout: 'main' }));
+	// Set view engine
+	app.set('view engine', 'hbs')
+		.set('views', __dirname + '/views')
+		.engine('hbs', hbs({ extname: 'hbs', defaultLayout: 'main' }));
 
-// Set static directory
-app.use(express.static(path.join(__dirname, '/public')))
-	.use(express.urlencoded({ extended: true }))
-	.use(home)
-	.use(register);
+	// Set static directory
+	app.use(express.static(path.join(__dirname, '/public')))
+		.use(express.urlencoded({ extended: true }))
+		.use(home)
+		.use(register);
 
-// Host server locally
-app.listen(port, () => {
-	console.log(`Listening on port: ${port}`);
-});
+	// Host server locally
+	app.listen(port, () => {
+		console.log(`Listening on port: ${port}`);
+	});
+};
+
+main();
